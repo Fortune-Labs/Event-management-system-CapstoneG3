943c72a995ef7b0edac47e0741a6caa581219643
Fortunatus Wulnye <wulnyefortunatus@gmail.com>
2020-09-02 13:43:44 +0000

    adds material ui

---
+++ .history/frontend/src/components/LoginSignup/register_20200902133333.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container">
+        <div className="header">Registration</div>
+        <div className="content">
+          <div className="form">
+            <div className="form-group">
+              <form>
+                <input type="text" name="fname" placeholder="First Name" />
+                <input type="text" name="lname" placeholder="Last Name" />{" "}
+                <br></br>
+                <input type="text" name="email" placeholder="Email" />
+                <input type="text" name="pwd" placeholder="Password" />
+                <br></br>
+                <input type="text" name="cpwd" placeholder="Confirm Password" />
+                <input type="text" name="address" placeholder="Address" />{" "}
+                <br></br>
+                <input type="text" name="phone" placeholder="Phone" />
+                <input type="text" name="city" placeholder="City" /> <br></br>
+                <label>
+                  Is going:
+                  <input
+                    name="isGoing"
+                    type="checkbox"
+                    checked={this.state.isGoing}
+                    onChange={this.handleInputChange}
+                  />
+                </label>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
