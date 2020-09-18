bbca0b1533164b0b5339ce1279ea97ad16b02e2b
Eben <68605403+Eprime30@users.noreply.github.com>
2020-09-18 05:03:32 +0000

    Fixed the validations on phone field and password in user_acc.serializers

--- backend/event/models.py
+++ backend/event/models.py
@@ -4,36 +4,6 @@ from user_account.models import Account
 
 # Create your models here.
 
-# class EventCreate(models.Model):
-
-#     def create_event(self, topic, times, room_capacity, speaker, tagline):
-#         """
-#         Create and save a Event with the given email and password.
-#         """
-#         if not topic:
-#             raise ValueError(_('The Topic name must be set'))
-#         if not times:
-#             raise ValueError(_('The Time of the Event must be set'))
-#         if not room_capacity:
-#             raise ValueError(_('The Room Capacity must be set'))
-#         if not speaker:
-#             raise ValueError(_('The Speaker for the event must be set'))
-#         if not tagline:
-#             raise ValueError(_('The Tagline for the event must be set'))
-
-#         event = self.model(
-#             topic=topic,
-#             times=times,
-#             room_capacity=room_capacity,
-#             speaker=speaker,
-#             tagline=tagline,
-
-#         )
-
-#         event.save(using=self._db)
-#         return event
-
-
 class Event(models.Model):
     topic = models.CharField(max_length=100)
     TIMES = (
