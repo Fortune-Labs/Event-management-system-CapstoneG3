e35681dc48af5f3c27f3f5e800c40b55620296af
Fortunatus Wulnye <wulnyefortunatus@gmail.com>
2020-09-20 17:30:56 +0000

    makes changes to main header

---
+++ .history/frontend/src/components/Mainheader/header_20200920101544.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="main-wrapper">
+        <div className="header-bar">
+          {/*   <div className="logo">
+            <a>
+              <img src="../Images/eventlogo.jpg" alt="#" />
+              CapstoneG3
+            </a>
+          </div> */}
+          <Link to="home">Home</Link>
+          <Link to="about">About</Link>
+          <Link to="Login">Login</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
