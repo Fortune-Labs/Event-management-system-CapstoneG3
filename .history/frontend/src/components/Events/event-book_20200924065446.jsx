f189e916660c3f4dcf422cecdc027cd317770ae1
Fortunatus Wulnye <wulnyefortunatus@gmail.com>
2020-09-24 07:49:02 +0000

    modifies frontend

---
+++ .history/frontend/src/components/Events/event-book_20200924065446.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+class InputForm extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="event-book-wrapper">
+        <div>
+          <form className="event-book-form-wrapper">
+            <h1>Booking Form</h1>
+
+            <div className="time">
+              Time:
+              <select>
+                <option>Morning</option>
+                <option>Mid-morning</option>
+                <option>Afternoon</option>
+              </select>
+            </div>
+
+            <button>Book</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InputForm;
