0c3cc849d0df5be7f57351b6a346135809e4077c
Eben <68605403+Eprime30@users.noreply.github.com>
2020-09-25 18:52:19 +0000

    edited user_account.models.py  - added line 4

--- backend/user_account/models.py
+++ backend/user_account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
